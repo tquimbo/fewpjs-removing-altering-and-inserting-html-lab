d8fbabea2a437e4bd21630e7058faf5a5a2b9e63
Tim Quimbo <tquimbo1@gmail.com>
2021-11-12 17:14:25 -0500

    commit tree 231a187f0ebef25da19df34f2985756560bfc0e2

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+document.querySelector('main#main').remove();
+
+let newHeader = document.createElement('h1')
+newHeader.id = "victory"
+newHeader.innerHTML = "Tim is the champion"
